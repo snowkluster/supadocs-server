c82b857f9ab5c21a748bd6a207ffc347d27da553
Rohan Kaushik <rohankaushik1@outlook.com>
2023-11-28 22:16:25 +0530

    build #1 submit for upload

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,12 @@
 import { Server } from "socket.io";
-import dotenv from "dotenv"
-
-dotenv.config()
-const port = 3000
+import dotenv from "dotenv";
+dotenv.config();
+const port = 3000;
 const io = new Server(port, {
     cors: {
         origin: 'https://localhost:5173',
         methods: ['GET', 'POST']
-    }   
+    }
 });
-
-io.on('connection', socket => {})
+io.on('connection', socket => { });
+//# sourceMappingURL=app.js.map
